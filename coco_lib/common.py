8c9b5aff40d898553993e9385bc5622ea1b28be7
Kevin Barnard <kbarnard@mbari.org>
2022-03-31 11:32:41 -0700

    Allow for missing image timestamp

--- coco_lib/common.py
+++ coco_lib/common.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from dataclasses import dataclass, field
+from typing import Optional
 
 from dataclasses_json import dataclass_json, config
 from marshmallow import fields
@@ -32,10 +33,10 @@ class Image:
     license: int
     flickr_url: str
     coco_url: str
-    date_captured: datetime = field(
+    date_captured: Optional[datetime] = field(
         metadata=config(
-            encoder=lambda d: d.strftime('%Y-%m-%d %H:%M:%S'),
-            decoder=lambda s: datetime.strptime(s, '%Y-%m-%d %H:%M:%S'),
+            encoder=lambda d: d.strftime('%Y-%m-%d %H:%M:%S') if d is not None else None,
+            decoder=lambda s: datetime.strptime(s, '%Y-%m-%d %H:%M:%S') if s is not None else None,
             mm_field=fields.DateTime(format='iso')
         )
     )
